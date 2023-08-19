1f15e4c05184b4a5d564a3febebdb082502c4eb3
Ali Ozzaim <125793435+Aliozzaim@users.noreply.github.com>
2023-08-19 18:55:44 +0200

    feat : add break point for responsive desing 320px

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@
 module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
+    screens: {
+      xs: "320px",
+      sm: "475px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1200px",
+    },
     extend: {
       lineHeight: {
         "extra-loose": "2.5",
