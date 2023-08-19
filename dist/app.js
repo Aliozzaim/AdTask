848c0a9cf0414a42cf360a996dfd2133211c5bf8
Ali Ozzaim <125793435+Aliozzaim@users.noreply.github.com>
2023-08-19 16:46:21 +0200

    Feat : Add Menu to the Header

--- dist/app.js
+++ dist/app.js
@@ -1,3 +1,30 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//   var grid = document.querySelector(".grid");
+//   var masonry = new Masonry(grid, {
+//     itemSelector: ".grid-item",
+//     columnWidth: 160,
+//   });
+
+//   function addMoreGridItems() {
+//     var gridItems = grid.querySelectorAll(".grid-item");
+//     var clonedItems = Array.from(gridItems).map(function (item) {
+//       return item.cloneNode(true);
+//     });
+
+//     clonedItems.forEach(function (clonedItem) {
+//       grid.appendChild(clonedItem);
+//     });
+
+//     masonry.reloadItems();
+//     masonry.layout();
+//   }
+
+//   var expandButton = document.getElementById("expandButton");
+//   expandButton.addEventListener("click", function () {
+//     addMoreGridItems();
+//   });
+// });
+
 document.addEventListener("DOMContentLoaded", function () {
   var grid = document.querySelector(".grid");
   var masonry = new Masonry(grid, {
@@ -5,18 +32,35 @@ document.addEventListener("DOMContentLoaded", function () {
     columnWidth: 160,
   });
 
-  function addMoreGridItems() {
-    var gridItems = grid.querySelectorAll(".grid-item");
-    var clonedItems = Array.from(gridItems).map(function (item) {
-      return item.cloneNode(true);
-    });
+  var imageUrls = [
+    "/images/Photo10.png",
+    "/images/Photo11.png",
+    "/images/Photo12.png",
+  ];
 
-    clonedItems.forEach(function (clonedItem) {
-      grid.appendChild(clonedItem);
+  function addMoreGridItems() {
+    var randomImageUrls = getRandomImageUrls(imageUrls, 3);
+
+    randomImageUrls.forEach(function (imageUrl) {
+      var gridItem = document.createElement("div");
+      gridItem.className = "grid-item";
+      var img = document.createElement("img");
+      img.src = imageUrl;
+      gridItem.appendChild(img);
+      grid.appendChild(gridItem);
     });
 
     masonry.reloadItems();
     masonry.layout();
+  }
+
+  function getRandomImageUrls(array, count) {
+    var shuffled = array.slice();
+    for (var i = shuffled.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+    }
+    return shuffled.slice(0, count);
   }
 
   var expandButton = document.getElementById("expandButton");
