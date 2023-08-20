a0b3aeda7cd9b318f58c32ba1069d76f705d71e2
Ali Ozzaim <125793435+Aliozzaim@users.noreply.github.com>
2023-08-20 23:33:37 +0200

    feat: refactor realizacje section

--- dist/app.js
+++ dist/app.js
@@ -1,12 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
   var grid = document.querySelector(".grid");
-  var masonry = new Masonry(grid, {
+  var masonry = new Masonry(".grid", {
     itemSelector: ".grid-item",
-    columnWidth: 160,
+    columnWidth: ".grid-sizer",
+    percentPosition: true,
+    gutter: ".gutter-sizer",
   });
 
   function addMoreGridItems() {
     var gridItems = grid.querySelectorAll(".grid-item");
+    var linearGradient = document.querySelector(".linear-gradient");
+    linearGradient.classList.add("opacity-0");
     var clonedItems = Array.from(gridItems).map(function (item) {
       return item.cloneNode(true);
     });
@@ -14,13 +18,16 @@ document.addEventListener("DOMContentLoaded", function () {
     clonedItems.forEach(function (clonedItem) {
       grid.appendChild(clonedItem);
     });
-
     masonry.reloadItems();
     masonry.layout();
+    setTimeout(() => {
+      grid.lastChild.scrollIntoViewIfNeeded();
+    }, 300);
   }
 
   var expandButton = document.getElementById("expandButton");
   expandButton.addEventListener("click", function () {
+    expandButton.classList.add("opacity-0");
     addMoreGridItems();
   });
 });
@@ -50,47 +57,3 @@ function showSlides(n) {
   }
   slides[slideIndex - 1].style.display = "block";
 }
-
-// document.addEventListener("DOMContentLoaded", function () {
-//   var grid = document.querySelector(".grid");
-//   var masonry = new Masonry(grid, {
-//     itemSelector: ".grid-item",
-//     columnWidth: 160,
-//   });
-
-//   var imageUrls = [
-//     "/images/Photo10.png",
-//     "/images/Photo11.png",
-//     "/images/Photo12.png",
-//   ];
-
-//   function addMoreGridItems() {
-//     var randomImageUrls = getRandomImageUrls(imageUrls, 3);
-
-//     randomImageUrls.forEach(function (imageUrl) {
-//       var gridItem = document.createElement("div");
-//       gridItem.className = "grid-item";
-//       var img = document.createElement("img");
-//       img.src = imageUrl;
-//       gridItem.appendChild(img);
-//       grid.appendChild(gridItem);
-//     });
-
-//     masonry.reloadItems();
-//     masonry.layout();
-//   }
-
-//   function getRandomImageUrls(array, count) {
-//     var shuffled = array.slice();
-//     for (var i = shuffled.length - 1; i > 0; i--) {
-//       var j = Math.floor(Math.random() * (i + 1));
-//       [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
-//     }
-//     return shuffled.slice(0, count);
-//   }
-
-//   var expandButton = document.getElementById("expandButton");
-//   expandButton.addEventListener("click", function () {
-//     addMoreGridItems();
-//   });
-// });
