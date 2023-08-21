06055d3ca32ffbcb4b80b9627c78941968cc27f3
Ali Ozzaim <125793435+Aliozzaim@users.noreply.github.com>
2023-08-21 02:50:32 +0200

    feat:seach bar animation

--- dist/app.js
+++ dist/app.js
@@ -30,6 +30,35 @@ document.addEventListener("DOMContentLoaded", function () {
     expandButton.classList.add("opacity-0");
     addMoreGridItems();
   });
+
+  const searchButton = document.querySelector(".search-input");
+  const navTop = document.querySelector(".nav-top");
+  const navTopItems = document.querySelectorAll(".nav-top-item");
+  console.log(navTopItems);
+  searchButton.addEventListener("focus", function () {
+    setTimeout(() => {
+      searchButton.style.width = navTop.offsetWidth + 24 + "px";
+      searchButton.placeholder = "Search";
+    }, 100);
+    for (let index = 0; index < navTopItems.length - 1; index++) {
+      const navTopItem = navTopItems[index];
+      navTopItem.style.opacity = 0;
+      setTimeout(() => {
+        navTopItem.style.display = "none";
+      }, 150);
+    }
+  });
+  searchButton.addEventListener("blur", function () {
+    searchButton.style.width = "24px";
+    searchButton.placeholder = "";
+    for (let index = 0; index < navTopItems.length - 1; index++) {
+      const navTopItem = navTopItems[index];
+      navTopItem.style.display = "flex";
+      setTimeout(() => {
+        navTopItem.style.opacity = 1;
+      }, 150);
+    }
+  });
 });
 //slider
 let slideIndex = 1;
@@ -57,3 +86,27 @@ function showSlides(n) {
   }
   slides[slideIndex - 1].style.display = "block";
 }
+
+// Resmi açma işlevi
+function openModal(imageId) {
+  var img = document.getElementById(imageId);
+  var modal = document.getElementById("myModal");
+  var modalImg = document.getElementById("modalImg");
+
+  modal.style.display = "block";
+  modalImg.src = img.src;
+}
+
+// Modal'ı kapatma işlevi
+function closeModal() {
+  var modal = document.getElementById("myModal");
+  modal.style.display = "none";
+}
+
+// Modal dışına tıklamayı kapatma işlevi
+window.onclick = function (event) {
+  var modal = document.getElementById("myModal");
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
